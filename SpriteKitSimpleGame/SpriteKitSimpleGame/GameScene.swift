a9eb036d686e579bee3d0c1e5efd57a45de115bb
Lidia Chou <lidia.chou94@gmail.com>
2015-05-04 11:04:47 -0300

    Moving monsters

--- SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
@@ -10,12 +10,54 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    //ADDING PLAYER
     let player = SKSpriteNode(imageNamed: "player")
     
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.whiteColor()
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         addChild(player)
+        
+        //Monstrinhos foreverrr!!!
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addMonster),
+                SKAction.waitForDuration(1.0)
+                ])
+            )
+        )
+        
+    }
+    
+    //ADDING MONSTERS
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(#min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster() {
+        //Create Sprite
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        //Determine where to spawn the monster along the Y axis
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        //Position the monster slightly off-screen along the right edge, and along a random position along the Y axis as calculated above
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        addChild(monster)
+        
+        //Speed of the monster
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        //Actions
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
     }
     
 }
